83cac334df0cc1823695754ee4c011d5131602f0
Blayne Chard <blayne@chard.com>
2019-10-10 20:00:06 +1300

    fix: sometimes paths are not easy to find the right folder

--- src/hyperfine/hyperfine.run.ts
+++ src/hyperfine/hyperfine.run.ts
@@ -3,10 +3,31 @@ import { promises as fs } from 'fs';
 import { HyperFineResult, HyperFineJsonOutput } from './hyperfine.output';
 import { randomBytes } from 'crypto';
 import { fileExists } from '../file';
+import * as path from 'path';
 
-export const HyperFineCommand = './static/hyperfine'; // tmp.tmpNameSync();
+/**
+ * Sometimes we can access hyperfine with ./
+ * sometimes we need __dirname, lets try both to be safe
+ */
+async function findHyperfine(): Promise<string> {
+    let HyperFineCommand = './static/hyperfine'; // tmp.tmpNameSync();
 
-async function waitForChildProcess(child: ChildProcess): Promise<string> {
+    let isHyperfineInstalled = await fileExists(HyperFineCommand);
+    if (isHyperfineInstalled) {
+        return HyperFineCommand;
+    }
+
+    const upCommand = path.join(__dirname, '..', HyperFineCommand);
+    let isHyperfineInstalledUp = await fileExists(upCommand);
+    if (isHyperfineInstalledUp) {
+        return upCommand;
+    }
+    throw new Error('Failed to open hyperfine @ ' + HyperFineCommand);
+}
+
+export async function waitForChildProcess(cmd: string): Promise<string> {
+    const child = exec(cmd);
+
     const output: string[] = [];
     if (child.stdout == null) {
         throw new Error('Child missing stdout');
@@ -25,16 +46,12 @@ async function waitForChildProcess(child: ChildProcess): Promise<string> {
 }
 
 export async function runHyperfine(cmd: string): Promise<HyperFineResult> {
-    let isHyperfineInstalled = await fileExists(HyperFineCommand);
-    if (!isHyperfineInstalled) {
-        throw new Error('Failed to open hyperfine @ ' + HyperFineCommand);
-    }
+    const HyperFineCommand = await findHyperfine();
 
-    const outputJsonFile = '/tmp/' + randomBytes(10).toString('hex') + '.json';
+    const outputJsonFile = './' + randomBytes(10).toString('hex') + '.json';
     const hyperfineExecute = [HyperFineCommand, `--export-json ${outputJsonFile}`, `'${cmd}'`]; // TODO escape \'
 
-    const output = exec(hyperfineExecute.join(' '));
-    const buffer = await waitForChildProcess(output);
+    const buffer = await waitForChildProcess(hyperfineExecute.join(' '));
     console.log(buffer);
 
     const outputJsonBuffer = await fs.readFile(outputJsonFile);
